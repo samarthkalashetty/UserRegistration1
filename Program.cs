02eddff61d238e1ad810c0196a3b9ed2016c7448
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-11 15:23:42 +0530

    Welcome to User Resgistration

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace UserRegistration1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            Console.ReadLine();
+        }
+    }
+}
